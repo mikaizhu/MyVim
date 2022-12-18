ab90a2a70823d2290f6b7e4f47e1a8cb2332884a
Mikai Zhu <59464933+mikaizhu@users.noreply.github.com>
2022-12-18 21:07:42 +0800

    Update nvim-treesitter.lua

--- config/nvim/lua/plugin-config/nvim-treesitter.lua
+++ config/nvim/lua/plugin-config/nvim-treesitter.lua
@@ -22,7 +22,7 @@ require'nvim-treesitter.configs'.setup {
   },
   -- 启用基于Treesitter的代码格式化(=) . NOTE: This is an experimental feature.
   indent = {
-    enable = true
+    enable = false
   }
 }
 -- 开启 Folding
